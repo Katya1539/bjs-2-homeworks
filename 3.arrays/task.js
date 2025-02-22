cc5bf2783781afc83b6f82083c2c9456623f4faa
Katya1539 <155986572+Katya1539@users.noreply.github.com>
2025-02-23 00:52:37 +0300

    Task 1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.lenght !== arr2.lenght) {
+    return false;
+  }
+
+  return arr1.every((value, index) => {
+    console.log(value, arr2[index]);
+
+    return value === arr2[index];
+  });
 }
+
+console.log(compareArrays([8, 9], [6, 7]))
+console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]))
+console.log(compareArrays([9, 2, 4, 8, 2], [9, 2, 4]))
+console.log(compareArrays([1, 2, 3], [2, 3, 1]))
+console.log(compareArrays([8, 1, 2, ], [8, 1, 2]))
 
 function getUsersNamesInAgeRange(users, gender) {
   
