fb1ad533e25744ffbca1443ac719ce17403d4f7a
Katya1539 <155986572+Katya1539@users.noreply.github.com>
2025-02-24 16:45:59 +0300

    Task 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,54 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+
+	this.setSubgect = function(subjectName) {
+		this.subject = subjectName;
+	};
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.addMarks = function(...marksToAdd) {
+	if (this.marks) {
+		console.log("Error: Student has been expelled.");
+		return;
+	}
+
+	if (!marksToAdd.length) {
+		this.marks.push(...marksToAdd);
+	}
+
+	return;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.getAverage = function() {
+
+	if (!this.marks || !this.marks.lenght) {
+		return 0;
+	}
+
+	const sum = this.marks.reduse((acc, curr) => acc + curr);
+	return sum / this.marks.length;
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
 
-Student.prototype.exclude = function (reason) {
-  
-}
+let student1 = new Student("Василиса", "женский", 19);
+console.log(student1.name);
+student1.setSubject("Algebra");
+console.log(student1.age)
+console.log(student1.getAverage());
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage());
+console.log(student1);
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
+console.log(student2.getAversge());
