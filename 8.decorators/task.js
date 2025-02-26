4ca0da01e45f602f1b6fc5ee5c8b986274002d61
Katya1539 <155986572+Katya1539@users.noreply.github.com>
2025-02-26 19:33:26 +0300

    Task 2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -35,7 +35,48 @@ upgraded(5, 2, 3);
 upgraded(6, 2, 3);
 upgraded(1, 2, 3);
 
-//Задача № 2
+
+
 function debounceDecoratorNew(func, delay) {
-  
+	let timeoutId;
+
+	wrapper.count = 0;
+	wrapper.allCount = 0;
+
+	function wrapper(...args) {
+		if (timeoutId) {
+			console.log('уже есть таймаут', args);
+			clearTimeout(timeoutId);
+		}
+		if (!timeoutId) {
+			console.log('первый сигнал', args);
+			func.call(this, ...args);
+			this.count++;
+		}
+		timeoutId = setTimeout(() => {
+			console.log('задержка больше 200млсек, сработал таймаут');
+
+			console.log('args', args);
+			func.apply(this, args);
+			wrapper.count++;
+		}, delay);
+
+		wrapper.allCount++;
+	}
+
+	return wrapper;
 }
+
+const sendSignal = (signalOrder, delay) => console.log('Сигнал отправлен', signalOrder, delay);
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+setTimeout(() => upgradedSendSignal(1, 0));
+setTimeout(() => upgradedSendSignal(2, 300), 300);
+setTimeout(() => upgradedSendSignal(3, 900), 900);
+setTimeout(() => upgradedSendSignal(4, 1200), 1200);
+setTimeout(() => upgradedSendSignal(5, 2300), 2300);
+setTimeout(() => upgradedSendSignal(6, 4400), 4400);
+setTimeout(() => upgradedSendSignal(7, 4500), 4500);
+setTimeout(() => {
+	console.log(upgradedSendSignal.count);
+	console.log(upgradedSendSignal.allCount);
+}, 7000)
